24134f7fe55ef72c37d2c3cc68703d0ed1dfcce7
apple1417 <git@apple1417.dev>
2021-06-25 21:43:14 +1200

    add OAK-PATCHWIN640-226

--- SteamManagerVersions.cs
+++ SteamManagerVersions.cs
@@ -720,7 +720,62 @@ namespace BL3SteamDownpatcher {
             "OakGame/Binaries/Win64/Borderlands3.exe",
           } }
         },
-      }
+      },
+
+      new VersionInfo() {
+        Name = "Unlocked Events",
+        ProductVersion = "OAK-PATCHWIN640-226",
+
+        Manifests = new Dictionary<Depot, ulong>() {
+          { Depot.Exe, 5244045638775298058 },
+          { Depot.Content, 8428033809087859114 },
+          { Depot.Dandelion, 9116600370569954686 },
+          { Depot.Hibiscus, 132348831915272916 },
+          { Depot.Geranium, 350750707932598048 },
+          { Depot.Alisma, 5627198090468988485 },
+          { Depot.Ixora, 988244885201248028 },
+          { Depot.Ixora2, 988244885201248028 },
+        },
+
+        FilesToAdd = new Dictionary<Depot, HashSet<string>>() {
+          { Depot.Content, new HashSet<string>() {
+            "OakGame/Content/Paks/pakchunk0-WindowsNoEditor_15_P.pak",
+            "OakGame/Content/Paks/pakchunk1-WindowsNoEditor_15_P.pak",
+            "OakGame/Content/Paks/pakchunk3-WindowsNoEditor_14_P.pak",
+            "OakGame/Content/Paks/pakchunk4-WindowsNoEditor_15_P.pak",
+            "OakGame/Content/Paks/pakchunk5-WindowsNoEditor_15_P.pak",
+            "OakGame/Content/Paks/pakchunk6-WindowsNoEditor_13_P.pak",
+            "OakGame/Content/Paks/pakchunk9-WindowsNoEditor_4_P.pak",
+            "OakGame/Content/Paks/pakchunk85-WindowsNoEditor_14_P.pak",
+            "OakGame/Content/Paks/pakchunk86-WindowsNoEditor_14_P.pak",
+            "OakGame/Content/Paks/pakchunk87-WindowsNoEditor_14_P.pak",
+            "OakGame/Content/Paks/pakchunk89-WindowsNoEditor_14_P.pak",
+            "OakGame/Content/Paks/pakchunk90-WindowsNoEditor_14_P.pak",
+            "OakGame/Content/Paks/pakchunk91-WindowsNoEditor_14_P.pak",
+          } }
+        },
+
+        FilesToReplace = new Dictionary<Depot, HashSet<string>>() {
+          { Depot.Exe, new HashSet<string>() {
+            "OakGame/Binaries/Win64/Borderlands3.exe",
+          } },
+          { Depot.Content, new HashSet<string>() {
+            "Engine/Binaries/ThirdParty/bifrost/x64/Release/bifrost.dll",
+            "Engine/Binaries/ThirdParty/cohtml/win64/cohtml.WindowsDesktop.dll",
+            "Engine/Binaries/ThirdParty/cohtml/win64/HttpServer.WindowsDesktop.dll",
+            "Engine/Binaries/ThirdParty/cohtml/win64/MediaDecoders.WindowsDesktop.dll",
+            "Engine/Binaries/ThirdParty/renoir/win64/RenoirCore.WindowsDesktop.dll",
+            "Engine/Binaries/ThirdParty/vivox/win64/vivoxsdk.dll",
+          } }
+        },
+
+        FilesToDelete = new Dictionary<Depot, HashSet<string>>() {
+          { Depot.Content, new HashSet<string>() {
+            "OakGame/Content/Movies/GBXLOGO.webm",
+            "OakGame/Content/Movies/2KLOGO.webm",
+          } }
+        },
+      },
     };
   }
 }
